7f4c98284085dd607bb0e61a288cb499a7a8b744
古梦 <2522407275@qq.com>
2023-09-04 14:55:24 +0800

    工作台业务模块

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -6,9 +6,13 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -62,5 +66,11 @@ public interface DishMapper {
     @Select("select * from dish where category_id = #{categoryId} and status = #{status}")
     List<Dish> list(Dish dish);
 
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 
 }
